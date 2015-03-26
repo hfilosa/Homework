b62755c7950fb5532e5edfa8cb5e1927612f2646
Henry Filosa <hfilosa@gmail.com>
2015-03-26 09:21:32 -0400

    Added an array queue with a fixed size and oen with a variable size

--- 11-Queues/Driver.java
+++ 11-Queues/Driver.java
@@ -1,5 +1,6 @@
 public class Driver{
     public static void main(String[] args){
+	/*
 	myQueue<Integer> queue = new myQueue<Integer>(0);
 	System.out.println(queue);
 	for (int i=1;i<10;i++){
@@ -8,6 +9,38 @@ public class Driver{
 	System.out.println(queue);
 	System.out.println(queue.top());
 	System.out.println(queue.dequeue());
+	System.out.println(queue);
+	queue.enqueue(0);
+	System.out.println(queue);
+	*/
+	/*
+	arrayQueue queue = new arrayQueue(0);
+	System.out.println(queue);
+	for (int i=1;i<10;i++){
+	    queue.enqueue(i);
+	}
+	System.out.println(queue);
+	System.out.println(queue.top());
+	System.out.println(queue.dequeue());
+	System.out.println(queue);
+	queue.enqueue(0);
+	System.out.println(queue);
+	for (int i=1;i<10;i++){
+	    queue.enqueue(i);
+	}
+	System.out.println(queue);
+	*/
+	fixedQueue queue = new fixedQueue(0,6);
+	System.out.println(queue);
+	for (int i=1;i<10;i++){
+	    queue.enqueue(i);
+	    System.out.println(queue);
+	}
+	System.out.println(queue);
+	System.out.println(queue.top());
+	System.out.println(queue.dequeue());
+	System.out.println(queue);
+	queue.enqueue(0);
 	System.out.println(queue);
     }
 }
