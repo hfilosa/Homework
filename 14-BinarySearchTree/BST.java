63e145b9cfce6a8b7d535d5824c8ae3d057825dc
Henry Filosa <henryfilosa@henrys-imac.home>
2015-04-26 00:26:13 -0400

    Fixed major issues with the searchand insertion algorithms. Created a method to generate a consistent queue of all the nodes on the tree. Now have to figure out how to use that to make a consistently readable display. MAybe start from the back?

--- 14-BinarySearchTree/BST.java
+++ 14-BinarySearchTree/BST.java
@@ -8,8 +8,6 @@ public class BST<E extends Comparable<E>>{
 	root = new node(data);
     }
 
-
-
     //Returns the node with data E
     public node<E> search(E data){
 	node<E> T=root;
@@ -17,8 +15,8 @@ public class BST<E extends Comparable<E>>{
 	while (T != null){
 	    c=T.getData().compareTo(data);
 	    if (c==0) return T;
-	    else if (c>0) T=T.getRight();
-	    else T=T.getLeft();
+	    else if (c>0) T=T.getLeft();
+	    else T=T.getRight();
 	}
 	return null;
     }
@@ -33,8 +31,8 @@ public class BST<E extends Comparable<E>>{
 	if (T==null) return null;
 	int c=T.getData().compareTo(data);
 	if (c==0) return T;
-	if (c>0) search(T.getRight(),data);
-	else search(T.getLeft(),data);
+	if (c>0) search(T.getLeft(),data);
+	else search(T.getRight(),data);
 	return null;
     }
 
@@ -48,8 +46,8 @@ public class BST<E extends Comparable<E>>{
 	    T2=T;
 	    c=T.getData().compareTo(data);
 	    if (c==0) return null;
-	    else if (c>0) T=T.getRight();
-	    else T=T.getLeft();
+	    else if (c>0) T=T.getLeft();
+	    else T=T.getRight();
 	}
 	return T2;
     }
@@ -63,9 +61,9 @@ public class BST<E extends Comparable<E>>{
     public node<E> parentSearch(node<E> T,E data){
 	int c=T.getData().compareTo(data);
 	if (c==0) return null;
-	if (c>0 && T.getRight()==null || c<0 && T.getLeft()==null) return T;
-	if (c>0) parentSearch(T.getRight(),data);
-	else parentSearch(T.getLeft(),data);
+	if (c>0 && T.getLeft()==null || c<0 && T.getRight()==null) return T;
+	if (c>0) parentSearch(T.getLeft(),data);
+	else parentSearch(T.getRight(),data);
 	return null;
     }
 
@@ -74,56 +72,68 @@ public class BST<E extends Comparable<E>>{
 	node<E> parent = rParentSearch(data);
 	node<E> leaf = new node(data);
 	int c=parent.getData().compareTo(data);
-	if (c>0) parent.setRight(leaf);
-	else if (c<0) parent.setLeft(leaf);
+	if (c>0) parent.setLeft(leaf);
+	else if (c<0) parent.setRight(leaf);
     }
 
     public void insert(E data){
 	node<E> parent = parentSearch(data);
 	node<E> leaf = new node(data);
 	int c=parent.getData().compareTo(data);
-	if (c>0) parent.setRight(leaf);
-	else if (c<0) parent.setLeft(leaf);
+	if (c>0) parent.setLeft(leaf);
+	else if (c<0) parent.setRight(leaf);
     }
     //returns depth of tree where 0 is a tree with only a root, 1 is a tree with one branch, etc.
-    public int rDepth(node<E> T){ 
-	if (T.getLeft()==null && T.getRight()==null) return 0;
-	if (T.getLeft()!=null) return 1+rDepth(T.getLeft());
-	if (T.getRight()!=null) return 1+rDepth(T.getRight());
-	return 0;
+    public int Depth(node<E> T){
+	int left=0,right=0; 
+	if (T.getLeft()!=null) left= 1+Depth(T.getLeft());
+	if (T.getRight()!=null) right= 1+Depth(T.getRight());
+	if(left>=right) return left;
+	else return right;
     }
 
     //wrapper for recursive depth finder
     public int depth(){
-	return rDepth(root);
+	return Depth(root);
     }
 
+    //returns a linkedlist of all the nodes in the tree
+    //nodes are ordered as if the tree where cut in slices from the top, cutting left to right
+    public LinkedList<node<E>> makeQueue(){
+	LinkedList<node<E>> ans = new LinkedList<node<E>>();
+	ans.add(root);
+	int depth=depth();
+	int n=0;
+	while (n<depth){
+	    int size=ans.size();
+	    for (int i=(int)Math.pow(2,n)-1;i<size;i++){
+		if (ans.get(i).getLeft()==null) ans.add(new node<E>(null));
+		else ans.add(ans.get(i).getLeft());
+		if (ans.get(i).getRight()==null) ans.add(new node<E>(null));
+		else ans.add(ans.get(i).getRight());
+	    }
+	    n++;
+	}
+	return ans;
+    }
+	    
     public String print(){
 	String ans="";
-	LinkedList<node<E>> queue = new LinkedList<node<E>>();
-	queue.add(root);
-	int depth = depth();
+	LinkedList<node<E>> queue = makeQueue();
 	int n=0;
+	int depth=depth();
 	String spaces="";
-	int temp;
 	node<E> test;
-	while (n<depth+1){
+	while (!queue.isEmpty()){
 	    spaces="";
-	    temp=queue.size();
-	    for (int i=0;i<temp;i++){	
-		if (queue.get(i).getRight()==null) queue.add(new node<E>(null));
-		else queue.add(queue.get(i).getRight());
-		if (queue.get(i).getLeft()==null) queue.add(new node<E>(null));
-		else queue.add(queue.get(i).getLeft());
-	    }
 	    for (int c=depth-n;c>0;c--){
 		spaces+=" ";
 	    }
 	    ans+=spaces;
 	    for (int c=(int)Math.pow(2,n);c>0;c--){
 		test = queue.pop();
-		if (test.getData()==null) ans+="  ";
-        	else ans+=test.getData()+" ";
+		if (test.getData()==null) ans+=" ";
+		else ans+=test.getData()+spaces;
 	    }
 	    ans+="\n";
 	    n++;
@@ -142,9 +152,7 @@ public class BST<E extends Comparable<E>>{
 	t.insert(4);
 	t.insert(6);
 	t.insert(19);
-	System.out.println(t.search(5));
-	System.out.println(t.search(4));
-	System.out.println(t.depth());
 	System.out.println(t.print());
+	System.out.println(t.makeQueue());
     }
 }
